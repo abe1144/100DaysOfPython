b546529f4bf713510c600412885738b073ed057c
Abraham Lin <abrahamlin55@gmail.com>
2021-03-31 19:14:10 -0400

    day2-tipCalculator

--- Day_2/tipCalculator.py
+++ Day_2/tipCalculator.py
@@ -11,5 +11,5 @@ split = int(input("How many people to split the bill? "))
 tip = int(input("What percentage tip would you like to give? "))
 
 # 5 - Output each individual's share total
-your_share = round((float(bill_total) / split) * (1 + float(tip)/100), 2)
+your_share = round((bill_total / split * (1 + tip/100)), 2)
 print("You should pay: ${}".format(your_share))
