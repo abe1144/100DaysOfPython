179d7684f74fdfaedee9aec726f2293e9ac4ea67
Abraham Lin <abrahamlin55@gmail.com>
2021-03-31 18:59:35 -0400

    day1-bandName Generator

---
+++ Day_1/bandName.py
@@ -0,0 +1,9 @@
+# 1 Greeting
+print("Hello There!")
+
+# 2 Ask for user's City
+city = input("What is the Name of Your City? ")
+# 3 Ask for user's pet name
+pet = input("What is the Name of Your Pet? ")
+# 4 combine the name of the city and pet to create the band name
+print("Your Band Name could be the {} {}".format(city, pet))
